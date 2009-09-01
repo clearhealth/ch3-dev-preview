dbc51c8cce6daccecced706e9f02094170d65343
root <root@domU-12-31-39-00-4D-F4.compute-1.internal>
2009-08-31 22:53:10 -0400

    starting sequence

--- clearhealth-3.0-inserts.sql
+++ clearhealth-3.0-inserts.sql
@@ -56,4 +56,4 @@ INSERT INTO `patient` (`person_id`, `is_default_provider_primary`, `default_prov
 
 INSERT INTO `person` (`person_id`, `salutation`, `last_name`, `first_name`, `middle_name`, `suffix`, `gender`, `initials`, `date_of_birth`, `summary`, `title`, `notes`, `email`, `secondary_email`, `has_photo`, `identifier`, `identifier_type`, `marital_status`, `inactive`, `active`, `primary_practice_id`) VALUES 
 (523523, '', 'Clearhealth', 'Test', 'J', '', 1, '', '1970-01-01', '', '', '', '', '', '0', '111223333', 0, 0, 0, 0, 0);
-
+INSERT INTO `sequences` ( `id` ) VALUES ( '1000000' );
