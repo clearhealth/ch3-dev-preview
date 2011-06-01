043bb57bc9013f5cc61a897bb4953d1f1e1ea3d0
devteam <info@clear-health.com>
2011-06-01 15:38:36 +0800

    upstream merge

--- clearhealth-3.0-inserts.sql
+++ clearhealth-3.0-inserts.sql
@@ -6,4 +6,5 @@ INSERT INTO `person` (`person_id`, `salutation`, `last_name`, `first_name`, `mid
 (1, '', 'Clearhealth', 'Test', 'J', '', 1, '', '1970-01-01', '', '', '', '', '', '0', '111223333', 0, 0, 0, 1, 0);
 INSERT INTO `sequences` ( `id` ) VALUES ( '1000000' );
 INSERT INTO `auditSequences` ( `id` ) VALUES ( '1' );
+INSERT INTO `record_sequence` ( `id` ) VALUES ( '10000' );
 
